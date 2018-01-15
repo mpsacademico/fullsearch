d2aea8a805756951b7e3a4b87737bd0954128cbd
Marcos Paulo da Silva <mpsacademico@gmail.com>
2018-01-15 11:12:06 -0200

    Criação da página inicial da aplicação

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "FullSearch"
+    return render_template('index.html', entries={})
 
 @app.route("/sobre/")
-def hello():
+def sobre():
     return "Sobre"
